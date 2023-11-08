59dca1593150cef97b57603033968b1db847fb67
Guilherme V. <imluizguilara@hotmail.com>
2023-11-08 12:14:32 -0300

    fix: Remove placeholder code for production

--- src/pages/ConfirmedOrder/index.tsx
+++ src/pages/ConfirmedOrder/index.tsx
@@ -1,25 +1,25 @@
-import { useNavigate } from 'react-router-dom';
-import { OrderData } from '../../@types/schemas/confirmOrderForm';
-import { useLocalStorage } from '../../hooks/useLocalStorage';
-import { FORM_STORAGE_KEY } from '../Checkout';
+import { useNavigate } from 'react-router-dom'
+import { OrderData } from '../../@types/schemas/confirmOrderForm'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { FORM_STORAGE_KEY } from '../Checkout'
 
 export const ConfirmedOrder = () => {
-  const { getItem } = useLocalStorage<OrderData>(FORM_STORAGE_KEY);
-  const data: OrderData | null = getItem();
+	const { getItem } = useLocalStorage<OrderData>(FORM_STORAGE_KEY)
+	const data: OrderData | null = getItem()
 
-  const navigate = useNavigate();
+	const navigate = useNavigate()
 
-  if (!data) {
-    navigate('/');
-    return <></>;
-  }
+	if (!data) {
+		navigate('/')
+		return <></>
+	}
 
-  const { rua, numero, bairro, cidade, uf } = data;
+	const { rua, numero } = data
 
-  return (
-    <div>
-      <span>{rua}</span>
-      <span>{numero}</span>
-    </div>
-  );
-};
+	return (
+		<div>
+			<span>{rua}</span>
+			<span>{numero}</span>
+		</div>
+	)
+}
